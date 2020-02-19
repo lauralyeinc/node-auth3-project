d25392baad3c2665dc2448a1d136533cebb82a1a
Laura <theimer.laura@gmail.com>
2020-02-18 21:51:01 -0500

    server running on 5000.

---
+++ api/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+
+// const authRouter = require('../auth/authRouter.js');
+// const usersRouter = require('../users/usersRouter.js');
+
+const server = express();
+
+server.use(express.json());
+
+// server.use('/api/auth', authRouter);
+// server.use('/api/users', usersRouter);
+
+server.get('/', (req,res) => {
+    res.send(`<h1> Node Auth 3 Project 👸 </h1>`)
+});
+
+
+// server.get('/token', (req, res) => {
+//     const payload = {
+//         subject: 'testuser',
+//         userid: 'tester',
+//         favoriteColor: 'purple'
+//     };
+
+//     const secret = "victoria's";
+
+//     const option = {
+//         expiresIn: '8h'
+//     };
+
+//     const token = jwt.sign(payload, secret, options);
+
+//     res.json(token); 
+// }); 
+
+module.exports = server; 
