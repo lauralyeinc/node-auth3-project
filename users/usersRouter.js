41fb7b78a0131f504ee3460c22dc97ebd81513d3
Laura <theimer.laura@gmail.com>
2020-02-18 21:59:43 -0500

    setting up usersModel.js and files for database.

---
+++ users/usersRouter.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+const Users = require('./usersModel.js'); // helper 
+const restricted = require('../auth/restrictedMW.js');  // middleware
+
+
+// getting allusers.  // w/out MW       //  /w MW
+router.get('/', (req, res) =>{
+    Users.find()
+        .then(allUsers => {
+            res.json(allUsers);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send(error);
+        });
+});
+
+
+module.exports = router; 
